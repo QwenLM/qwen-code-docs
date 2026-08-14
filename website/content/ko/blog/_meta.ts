f7d2706a774c639b2190e7b7fbb4711cca78d171
live-alife <yunyiya111@gmail.com>
2026-08-14 18:49:16 +0800

    docs: add weekly-update-2026-08-13 blog in 7 languages

---
+++ website/content/ko/blog/_meta.ts
@@ -0,0 +1,11 @@
+export default {
+  index: {
+    title: '블로그',
+    theme: {
+      layout: 'full',
+      sidebar: false,
+      toc: false,
+    },
+  },
+  updates: '기능 업데이트',
+};
---
+++ website/content/ko/blog/updates/_meta.ts
@@ -0,0 +1,4 @@
+export default {
+  index: { display: 'hidden' },
+  'weekly-update-2026-08-13': '08-13 제품 업데이트',
+};
